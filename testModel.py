7d0c475530b6e9bd0b19dde80045978fdb0a9b42
Ynnck123 <3reiss@informatik.uni-hamburg.de>
2018-06-14 14:47:54 +0200

    TestModel.py Sachen

--- testModel.py
+++ testModel.py
@@ -1,8 +1,56 @@
 # MLP for Pima Indians Dataset Serialize to JSON and HDF5
+import numpy as np
 from keras.models import model_from_json
-from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from numpy import array
+from keras.preprocessing.text import Tokenizer
+from os import listdir
+from string import punctuation
+
+
+# load doc into memory
+def load_doc(filename):
+    # open the file as read only
+    file = open(filename, 'r')
+    # read all text
+    text = file.read()
+    # close the file
+    file.close()
+    return text
+
+
+# turn a doc into clean tokens
+def clean_doc(doc, vocab):
+    # split into tokens by white space
+    tokens = doc.split()
+    # remove punctuation from each token
+    table = str.maketrans('', '', punctuation)
+    tokens = [w.translate(table) for w in tokens]
+    # filter out tokens not in vocab
+    tokens = [w for w in tokens if w in vocab]
+    tokens = ' '.join(tokens)
+    return tokens
+
+
+# load all docs in a directory
+def process_docs(directory, vocab, is_trian):
+    documents = list()
+    # walk through all files in the folder
+    for filename in listdir(directory):
+        # skip any reviews in the test set
+        if is_trian and filename.startswith('cv9'):
+            continue
+        if not is_trian and not filename.startswith('cv9'):
+            continue
+        # create the full path of the file to open
+        path = directory + '/' + filename
+        # load the doc
+        doc = load_doc(path)
+        # clean doc
+        tokens = clean_doc(doc, vocab)
+        # add to list
+        documents.append(tokens)
+    return documents
+
 
 # load json and create model
 json_file = open('model.json', 'r')
@@ -15,33 +63,30 @@ model.load_weights("model.h5")
 
 print("Loaded model from disk")
 
-# load label names to use in prediction results
-testphrase = "Der Schauspieler hat eine gute Arbeit geleistet"
-
 # load the vocabulary
-vocab = testphrase.split()
+vocab_filename = 'vocab.txt'
+vocab = load_doc(vocab_filename)
+vocab = vocab.split()
 vocab = set(vocab)
 
-# load all training reviews
-train_docs = vocab
+# load the test data and process it
+testDataFolder = 'testfolder'
+testdataDoc = load_doc(testDataFolder)
+test_docs = process_docs(testdataDoc, vocab, True)
 
 # create the tokenizer
 tokenizer = Tokenizer()
-# fit the tokenizer on the documents
-tokenizer.fit_on_texts(train_docs)
+
+# fit the tokenizer on the test data
+tokenizer.fit_on_texts(test_docs)
 
 # sequence encode
-encoded_docs = tokenizer.texts_to_sequences(train_docs)
+encoded_docs = tokenizer.texts_to_sequences(test_docs)
+
 # pad sequences
-max_length = max([len(s.split()) for s in train_docs])
-XTest = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
+max_length = max([len(s.split()) for s in test_docs])
+Xtest = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
 
-# define vocabulary size (largest integer value)
-vocab_size = len(tokenizer.word_index) + 1
+y_sentiment = model.predict(np.array(Xtest))
 
-testIt = numpy.array(XTest)
-
-# predict the probability across all output classes
-y_proba = model.predict(XTest)
-
-print(y_proba)
+print(y_sentiment)
